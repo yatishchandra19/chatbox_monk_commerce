64b3e9e7b93cfcca3c8310641fb250b04a93cc85
yatishchandra19 <yatishchandra19@gmail.com>
2024-06-14 20:24:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import { data } from "./data/Data";
 
 function App() {
   const [contacts, setContacts] = useState(data);
+  const [selectedContact, setSelectedContact] = useState(null);
+
+  const handleSelectContact = (contactId) => {
+    const contact = contacts.find((contact) => contact.userId === contactId);
+    if (contact) {
+      setSelectedContact(contact);
+    }
+  };
 
   return (
     <div className="App grid grid-cols-4 h-screen bg-white">
-      <div className="col-span-1 h-full p-5">
-        <ContactList contacts={contacts} />
+      {selectedContact && <p>{selectedContact}</p>}
+      <div
+        className="col-span-1 h-full p-5"
+        style={{ borderRight: "1px solid #EFEFEF" }}
+      >
+        <ContactList
+          contacts={contacts}
+          onSelectContact={handleSelectContact}
+        />
       </div>
       <div className="col-span-3 h-full">
         <Conversation />
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,14 +1,33 @@
 import React from "react";
+import "../index.css";
 
 import { ReactComponent as Dots } from "../icons/dots.svg";
 
-function Contact({ dp, name, latestMessage }) {
+function Contact({ onSelectContact, userId, dp, name, latestMessage }) {
   return (
-    <div className="p-2 flex items-center space-x-3 bg-gray-400">
+    <div
+      onClick={() => onSelectContact(contact.userId)}
+      className="py-2 px-1 flex items-center space-x-3 mb-2"
+    >
       <img src={dp} className="h-14 w-14 rounded-full flex-shrink-0 "></img>
       <div className="flex-grow">
-        <p>{name}</p>
-        <p className="">{latestMessage}</p>
+        <p
+          className="font-semibold text-base"
+          style={{
+            fontFamily: "-apple-system, BlinkMacSystemFont, sans-serif",
+          }}
+        >
+          {name}
+        </p>
+        <p
+          className="font-normal h-6 overflow-clip text-sm"
+          style={{
+            fontFamily: "-apple-system, BlinkMacSystemFont, sans-serif",
+            color: "#8E8E93",
+          }}
+        >
+          {latestMessage}
+        </p>
       </div>
       <div className="px-2 flex-shrink-0">
         <Dots />
@@ -17,14 +36,16 @@ function Contact({ dp, name, latestMessage }) {
   );
 }
 
-function ContactList({ contacts }) {
+function ContactList({ contacts, handleSelectContact }) {
   console.log(contacts[0].profilePictureURL);
   return (
     <div className="">
-      <div className="font-normal text-xl leading-6">Chats</div>
+      <div className="font-normal font-sans text-xl leading-6">Chats</div>
       <div className="py-2 ">
         {contacts.map((contact) => (
           <Contact
+            onSelectContact={handleSelectContact}
+            userId={contact.userId}
             dp={contact.profilePictureURL}
             name={contact.name}
             latestMessage={contact.chat[contact.chat.length - 1].you.message}
